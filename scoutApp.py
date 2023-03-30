c49928851843f17913569d0b9c31963f3b392dfd
Jess Elmhurst <88048455+elmhuje713@users.noreply.github.com>
2023-03-30 00:08:39 -0400

    Add files via upload

---
+++ scoutApp.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app_version = '1.1.0'
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+#  return render_template('form.html')
+  return render_template('home.html')
+
+if __name__ == '__main__':
+  app.run(debug = True)
